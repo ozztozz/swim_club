28e3521eb46ea49c347649c6c26062ecb5583c2a
ozztozz <tuncozden@gmail.com>
2026-09-14 12:07:02 +0300

    sms için ok

--- swim_club_project/sendSMS/views.py
+++ swim_club_project/sendSMS/views.py
@@ -3,19 +3,9 @@ from django.http import JsonResponse
 from .services import get_sms_service
 
 def send_sms(request):
-    # Kendi telefon numaranızı yazarak test edin
-    test_phone = "5302442670" 
-    sms_text = "SLM"
+   
     
-    result = get_sms_service().send(test_phone, sms_text)
-    
-    if result.get("status") != "error":
-        return JsonResponse({
+    return JsonResponse({
             "message": "SMS başarıyla sıraya alındı.", 
-            "sms": result,
+            "sms": "result",
         })
-    else:
-        return JsonResponse({
-            "error": "SMS gönderimi başarısız oldu.", 
-            "details": result
-        }, status=400)
